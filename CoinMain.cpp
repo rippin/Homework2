3f3dea03cb75445ee20bf9a75e37a6a1c2acb188
rippin <efisrippin@ive.com>
2015-10-15 10:07:30 -0700

    Final Commit

--- CoinMain.cpp
+++ CoinMain.cpp
@@ -9,39 +9,31 @@
 // CoinMain.cpp  /// The name of this file
 #include "Coins.h"
 #include <iostream>
-const int CENTS_FOR_CANDYBAR = 482;
 
 using namespace std;
 
 int main() {
     
-    ///  The first line creates a Coins object called 'pocket.'
-    Coins pocket( 100, 10, 10, 100 );
-    cout << "I started with " << pocket << " In my pocket." << endl;
-    ///  This line creates a Coins object called payForCandy and initializes it.
-    Coins payForCandy = pocket.extractChange( CENTS_FOR_CANDYBAR );
-    cout << "I bought a candy bar for " << CENTS_FOR_CANDYBAR
-    << " cents using " << payForCandy << endl;
-    cout << "I have " << pocket << " left in my pocket." << endl;
-    
+    Coins pocket(5, 3, 6, 8);
     Coins piggyBank(50,50,50,50);
     pocket.extractChange(68);
     
-    cout << "I have bought some chips and now have " << pocket <<endl;
+    cout << "I have bought some chips and now have " << pocket << " in my pocket." << endl;
     
+    cout << "Depositing from piggybank into pocket.\n";
     
-        cout << "Depositing pocket into piggybank.\n";
-    piggyBank.depositChange(49, 5, 8, 8, pocket);
+    pocket.depositChange(20, 5, 8, 8, piggyBank);
 
     
-    cout << "Piggybank contains  " << piggyBank << endl;
+    cout << "Piggybank contains " << piggyBank << endl;
     cout << "Pocket contains " << pocket << endl;
     
     Coins sofa(2,3,5, 20);
     
     cout << "Found " << sofa << " in the sofa" << endl;
     
-    sofa.depositChange(pocket);
+    piggyBank.depositChange(sofa);
+        cout << "Depositing sofa into piggybank which now contains  " << piggyBank << endl;
     
     return 0;
 }
